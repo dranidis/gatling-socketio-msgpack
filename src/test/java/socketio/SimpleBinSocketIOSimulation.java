5d42303003a77085e2c06ba0870d3b466c532aad
D Dranidis <dranidis@gmail.com>
2023-11-18 14:14:28 +0200

    Add inner class Packet in Bin

--- src/test/java/socketio/SimpleBinSocketIOSimulation.java
+++ src/test/java/socketio/SimpleBinSocketIOSimulation.java
@@ -4,18 +4,25 @@ import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.msgpack.jackson.dataformat.MessagePackFactory;
 import org.msgpack.jackson.dataformat.MessagePackMapper;
 
 import io.gatling.javaapi.core.*;
 import io.gatling.javaapi.http.*;
 
 /**
- * Run with the msgpack-express-socketio-server node client in index.js
+ * Connects to a socket-io server that uses the msgpack parser.
+ * <p>
+ * For an example server, run with the msgpack-express-socketio-server node
+ * client in index.js
  */
 public class SimpleBinSocketIOSimulation extends Simulation {
 
@@ -25,83 +32,84 @@ public class SimpleBinSocketIOSimulation extends Simulation {
       .wsMaxReconnects(5)
       .wsAutoReplySocketIo4();
 
-  byte[] connectionBytes;
-  byte[] messageBytes;
-  byte[] disconnectBytes;
+  byte[] packConnectBytes;
+  byte[] packSendBytes;
+  byte[] packDisconnectBytes;
+
+  // Instantiate ObjectMapper for MessagePack
+  ObjectMapper objectMapper = new ObjectMapper(new MessagePackFactory());
+
+  class Packet {
+    public int type;
+    public String nsp;
+    public List<String> data;
+
+    public Packet(int type, String nsp, List<String> data) {
+      this.type = type;
+      this.nsp = nsp;
+      this.data = data;
+    }
+  }
 
   {
-    // Instantiate ObjectMapper for MessagePack
-    ObjectMapper objectMapper = new MessagePackMapper();
-
-    // Serialize a Map to byte array
-    Map<String, Object> connectionMap = new HashMap<>();
-    connectionMap.put("type", 0);
-    connectionMap.put("nsp", "/");
-
-    Map<String, Object> messageMap = new HashMap<>();
-    messageMap.put("type", 2);
-    messageMap.put("data", new String[] { "message", "Hi" });
-    messageMap.put("nsp", "/");
-
-    Map<String, Object> disconnectMap = new HashMap<>();
-    disconnectMap.put("type", 1);
-    disconnectMap.put("nsp", "/");
-
     try {
-      connectionBytes = objectMapper.writeValueAsBytes(connectionMap);
-      messageBytes = objectMapper.writeValueAsBytes(messageMap);
-      disconnectBytes = objectMapper.writeValueAsBytes(disconnectMap);
-    } catch (Exception e) {
+      packConnectBytes = objectMapper.writeValueAsBytes(new Packet(0, "/", Arrays.asList()));
+      packSendBytes = objectMapper.writeValueAsBytes(new Packet(2, "/", Arrays.asList("message", "hi")));
+      packDisconnectBytes = objectMapper.writeValueAsBytes(new Packet(1, "/", Arrays.asList()));
+    } catch (JsonProcessingException e) {
       e.printStackTrace();
     }
   }
 
+  CheckBuilder checkBroadcastEventSaveMessage = bodyBytes().transform(bytes -> {
+    // Deserialize the byte array to a Map
+    Map<String, Object> deserialized = null;
+    ObjectMapper objectMapper = new MessagePackMapper();
+
+    try {
+      deserialized = objectMapper.readValue(bytes, new TypeReference<Map<String, Object>>() {
+      });
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    /**
+     * first element in the data array is the event name and the second element is
+     * the message
+     */
+    @SuppressWarnings("unchecked")
+    List<String> dataList = (List<String>) deserialized.get("data");
+    String eventName = dataList.get(0);
+    String message = dataList.get(1);
+
+    if (!eventName.equals("broadcast")) {
+      throw new RuntimeException("Expected broadcast event, got " + eventName + " instead");
+    }
+
+    return message;
+  }).saveAs("response");
+
   ScenarioBuilder scene = scenario("WebSocket")
       .exec(ws("Connect WS").connect("/socket.io/?EIO=4&transport=websocket"))
       .pause(1)
-      .exec(ws("Connect to Socket.IO")
-          .sendBytes(connectionBytes))
+      .exec(ws("Connect to Socket.IO").sendBytes(packConnectBytes))
 
       .pause(1)
-      .exec(ws("Say hi")
-          .sendBytes(messageBytes)
+      .exec(ws("Say hi").sendBytes(packSendBytes)
 
           .await(30).on(
-              ws.checkBinaryMessage("checkMessage")
-                  .check(bodyBytes().transform(bytes -> {
-
-                    // Deserialize the byte array to a Map
-                    Map<String, Object> deserialized = null;
-                    ObjectMapper objectMapper = new MessagePackMapper();
-
-                    try {
-                      deserialized = objectMapper.readValue(bytes, new TypeReference<Map<String, Object>>() {
-                      });
-                    } catch (IOException e) {
-                      e.printStackTrace();
-                    }
-
-                    @SuppressWarnings("unchecked")
-                    List<String> dataList = (List<String>) deserialized.get("data");
-                    String msg = dataList.get(1);
-
-                    return msg;
-                  }).saveAs("response"))))
+              ws.checkBinaryMessage("check broadcast event in message")
+                  .check(checkBroadcastEventSaveMessage)))
 
       .exec(session -> {
         System.out.println("response: " + session.get("response"));
         return session;
-      }).exec(ws("Disconnect from Socket.IO")
-
-          .sendBytes(disconnectBytes))
-
-      // .sendText("41"))
-
-      .exec(ws("Close WS").close())
-  //
-  ;
+      })
+      .exec(ws("Disconnect from Socket.IO").sendBytes(packDisconnectBytes))
+      .exec(ws("Close WS").close());
 
   {
+
     setUp(scene.injectOpen(atOnceUsers(1))).protocols(httpProtocol);
   }
 }
